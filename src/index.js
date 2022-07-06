3084df55fee7980b99c1afc2fea725b70789b61a
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-07-06 12:30:00 +0500

    Fixed Linter Errors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-import _ from 'lodash';
 import './style.css';
 import refresh from './refresh.png';
 import enter from './enter.png';
@@ -28,37 +27,33 @@ const ToDoListData = [
 ];
 
 const getImage = () => {
-  const imageHolder = document.getElementById("imageRefresh");
+  const imageHolder = document.getElementById('imageRefresh');
   imageHolder.src = refresh;
 
-  const imageEnter = document.getElementById("imageEnter");
+  const imageEnter = document.getElementById('imageEnter');
   imageEnter.src = enter;
-}
+};
 
-
- const TodoComponent = () => {
-  const ul = document.getElementById("list");
-  for(let i = 0; i< ToDoListData.length; i++){
-    let li = document.createElement('li');
-    let cbx = document.createElement('input');
-    let span = document.createElement('span');
-    let anchor = document.createElement('a');
-    let img = document.createElement('img');
-    
-    cbx.type = "checkbox";
+const TodoComponent = () => {
+  const ul = document.getElementById('list');
+  for (let i = 0; i < ToDoListData.length; i += 1) {
+    const li = document.createElement('li');
+    const cbx = document.createElement('input');
+    const span = document.createElement('span');
+    const anchor = document.createElement('a');
+    const img = document.createElement('img');    
+    cbx.type = 'checkbox';
     cbx.checked = ToDoListData[i].completed;
     cbx.id = ToDoListData[i].index;
-
     img.src = expand;
     anchor.appendChild(img);
-
     span.innerHTML = ToDoListData[i].description;
     li.appendChild(cbx);
     li.appendChild(span);
     li.appendChild(anchor);
     ul.appendChild(li);
   }
- }
+ };
 
- TodoComponent();
- getImage();
+TodoComponent();
+getImage();
--- src/print.js
+++
@@ -1,5 +0,0 @@
-import ToDoList from './Data.js';
-
-export default function printData() {
-  console.log('I get called from print.js!');
-}
