156bd5ae21ba7c6318b6f278d7d3f0d75fe0f177
AsimKhan2019 <asim.mahmood.khan@hotmail.com>
2022-07-08 16:41:56 +0500

    Completed single delete and update indexes

--- src/operations.js
+++ src/operations.js
@@ -1,5 +1,6 @@
 import Storage from './storage.js';
 import expand from './expand.png';
+import trash from './trash.png';
 
 export default class Operations {
   static getTaskList() {
@@ -14,12 +15,17 @@ export default class Operations {
       const img = document.createElement('img');
       span.type = 'text';
       span.id = taskList[i].index;
+      li.id = taskList[i].index;
+      anchor.id = taskList[i].index;
       cbx.type = 'checkbox';
       cbx.checked = taskList[i].completed;
       cbx.id = taskList[i].index;
       cbx.classList.add('checkbox');
       cbx.addEventListener('change', (e) => this.cbxHandler(e));
       span.addEventListener('change', (e) => this.updateValue(e));
+      span.addEventListener('click', (e) => this.changeIcon(e));
+      span.addEventListener('blur', (e) => this.resetIcon(e));
+      anchor.addEventListener('click', (e) => this.deleteFromList(e));
       img.src = expand;
       anchor.appendChild(img);
       span.value = taskList[i].description;
@@ -57,6 +63,25 @@ export default class Operations {
     Operations.getTaskList();
   }
 
+  static deleteFromList(e) {
+    const index = parseInt(e.target.parentElement.id, 10);
+    const taskList1 = Storage.getData('taskList');
+    const temp1 = taskList1.filter((item) => item.index !== index);
+    Storage.saveData(temp1);
+
+    const taskList = Storage.getData('taskList');
+    const temp = taskList;
+    
+    let i = 0;
+    temp.forEach((element) => {
+      i += 1;
+      element.index = i;
+    });
+
+    Storage.saveData(temp);
+    Operations.getTaskList();
+  }
+
   static cbxHandler(e) {
     const index = parseInt(e.target.id, 10);
     const taskListObj = Storage.getData('taskList');
@@ -71,5 +96,14 @@ export default class Operations {
     const task = taskListObj.find((task) => task.index === index);
     task.description = e.target.value;
     Storage.saveData(taskListObj);
+  }
+
+  static changeIcon(e) {
+    const index = parseInt(e.target.id, 10);
+    e.target.parentElement.querySelector("img").src = trash;
+  }
+
+  static resetIcon(e){
+    e.target.parentElement.querySelector("img").src = expand;
   }
 }
