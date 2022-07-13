2ed01105080628eb748212557ed58df5930a01c5
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-13 16:35:37 +0200

    add tests for removing item

--- src/test/operations.test.js
+++ src/test/operations.test.js
@@ -44,3 +44,53 @@
     expect(tasks).toHaveLength(2);
   })
  });
+
+ describe('Testing for removing items', () => {
+  test('Removing 1 item', () => {
+    localStorage.clear()
+    document.body.innerHTML = 
+    '<div>' +
+    '  <ul id="list"></li>' +
+    '</div>';
+
+    const data = {
+      index: 0,
+      description: 'This is just a sample',
+      completed: false
+    };
+    Operations.addToList(data);
+    let anchor = document.querySelector('a')
+    Operations.deleteFromList(anchor)
+    const tasks = document.querySelectorAll('#list li');
+    expect(tasks).toHaveLength(0);
+  })
+
+  test('Removing 2 items', () => {
+    localStorage.clear()
+    document.body.innerHTML = 
+    '<div>' +
+    '  <ul id="list"></li>' +
+    '</div>';
+
+    const data = {
+      index: 0,
+      description: 'This is just a sample',
+      completed: false
+    };
+
+    const data1 = {
+      index: 1,
+      description: 'This is just a sample',
+      completed: false
+    };
+
+    Operations.addToList(data);
+    Operations.addToList(data1)
+    let anchor = document.querySelector('a')
+    Operations.deleteFromList(anchor)
+    let anchor1 = document.querySelector('a')
+    Operations.deleteFromList(anchor1)
+    const tasks = document.querySelectorAll('#list li');
+    expect(tasks).toHaveLength(0);
+  })
+ })
