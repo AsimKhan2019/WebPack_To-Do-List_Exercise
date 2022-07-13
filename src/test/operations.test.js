f5cb6555e100babb376c2936f2e90af1c1b07645
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-13 16:21:52 +0200

    add second test for the adding function

--- src/test/operations.test.js
+++ src/test/operations.test.js
@@ -3,10 +3,9 @@
  */
 
  import Operations from '../operations';
- //import addDatatoList from '../index';
 
  describe('Testing for adding items', () => {
-  test('Add items to list', () => {
+  test('Add 1 item to list', () => {
     document.body.innerHTML = 
     '<div>' +
     '  <ul id="list"></li>' +
@@ -20,5 +19,28 @@
     Operations.addToList(data);
     const tasks = document.querySelectorAll('#list li');
     expect(tasks).toHaveLength(1);
+  })
+
+  test('Add 2 items to list', () => {
+    localStorage.clear()
+    document.body.innerHTML = 
+    '<div>' +
+    '  <ul id="list"></li>' +
+    '</div>';
+
+    const data = {
+      index: 0,
+      description: 'This is just a sample',
+      completed: false
+    };
+    const data1 = {
+      index: 1,
+      description: 'This is just another sample',
+      completed: false
+    };
+    Operations.addToList(data);
+    Operations.addToList(data1);
+    const tasks = document.querySelectorAll('#list li');
+    expect(tasks).toHaveLength(2);
   })
  });
