5ebfb842a45d972ee9cf00f2df50b0347bdffeea
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-07-14 10:21:53 +0200

    add test for clear all and another test for update value

--- src/test/operationUpdates.test.js
+++ src/test/operationUpdates.test.js
@@ -30,6 +30,40 @@
     const item = Storage.getData('taskList');
     expect(item[0].description).toBe(newData);
   })
+
+  test('Update 2 values', () => {
+    localStorage.clear();
+
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></li>'
+    + '</div>';
+    
+    const data = {
+      index: 0, 
+      description: 'This is just a test',
+      completed: false
+    };
+    const data1 = {
+      index: 1, 
+      description: 'Anything',
+      completed: false
+    };
+
+    Operations.addToList(data);
+    Operations.addToList(data1);
+    const getData = document.querySelectorAll("#list li");
+    const inputId0 = getData[0].children[1];
+    const inputId1 = getData[1].children[1];
+    inputId0.value = "This is updated";
+    inputId1.value = "This is not updated";
+
+    Operations.updateValue(inputId0);
+    Operations.updateValue(inputId1);
+    Operations.getTaskList();
+
+    const item = Storage.getData('taskList');
+    expect(item[0].description + item[1].description).toBe("This is updatedThis is not updated");
+  })
  });
 
  describe('Updating the completed status', () => {
@@ -56,5 +90,73 @@
 
     const item = Storage.getData('taskList');
     expect(item[0].completed).toBe(true);
+  })
+ });
+
+ describe('Clearing all completed', () => {
+  test('Update status', () => {
+    localStorage.clear();
+
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></li>'
+    + '</div>';
+    
+    const data = {
+      index: 0, 
+      description: 'This is just a test',
+      completed: true,
+    };
+    const data1 = {
+      index: 1, 
+      description: 'This is just a test',
+      completed: true,
+    };
+    const data2 = {
+      index: 2, 
+      description: 'This is just a test',
+      completed: false
+    };
+    
+
+    Operations.addToList(data);
+    Operations.addToList(data1);
+    Operations.addToList(data2);
+
+    Operations.removeFromList();
+    const item = Storage.getData('taskList');
+    expect(item[0].completed).toBe(false);
+  })
+
+  test('Update status', () => {
+    localStorage.clear();
+
+    document.body.innerHTML = '<div>'
+    + '  <ul id="list"></li>'
+    + '</div>';
+    
+    const data = {
+      index: 0, 
+      description: 'This is just a test',
+      completed: false,
+    };
+    const data1 = {
+      index: 1, 
+      description: 'This is just a test',
+      completed: true,
+    };
+    const data2 = {
+      index: 2, 
+      description: 'This is just a test',
+      completed: false
+    };
+    
+
+    Operations.addToList(data);
+    Operations.addToList(data1);
+    Operations.addToList(data2);
+
+    Operations.removeFromList();
+    const item = Storage.getData('taskList');
+    expect(item[0].completed && item[1].completed).toBe(false);
   })
  });
